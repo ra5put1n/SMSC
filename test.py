a142ed7308f23970730fd2d98b8e1fc797ac105d
Krishna :) <35192026+ra5put1n@users.noreply.github.com>
2020-10-29 14:11:35 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import os
 os.system('ipcrm -a')
+os.system("touch request.txt")
+os.system("touch answer.txt")
 os.system("gcc -o server server.c")
 os.system("gcc -o client client.c")
 os.system("gcc -o service1 service1.c")
