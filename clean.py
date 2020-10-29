e4c7eb73a52160ba1b48c77b228b7935ab6e3ac5
Krishna Kaipa <iit2019203@iiita.ac.in>
2020-10-29 17:32:24 +0000

    modified test and added clean.py

---
+++ clean.py
@@ -0,0 +1,8 @@
+import os
+os.system("rm request.txt")
+os.system("rm answer.txt")
+os.system("rm server")
+os.system("rm client")
+os.system("rm service1")
+os.system("rm service2")
+os.system("rm service3")
